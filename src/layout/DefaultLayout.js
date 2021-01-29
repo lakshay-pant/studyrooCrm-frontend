5241cc7b16a7a5ca8655feb98801532c92e072ff
Lakshay <pantlakshay2@gmail.com>
2021-01-29 17:13:50 +0530

    add sidebar

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,12 +1,65 @@
-import React from 'react'
-import {SideBar} from "./sidebar/SideBar"
+import React from 'react';
+import * as s from "./App.styles";
+import SideBar from "./sidebar/SideBar"
+import * as Palette from './colors'
+
 
 export const DefaultLayout = ({children}) => {
+    const backgroundImage = 'images/mountain.jpg';
+  const sidebarHeader = {
+    fullName: 'Yo Yo Travel',
+    shortName: 'YO'
+  };
+  const menuItems = [
+    {name: 'Dashboard', to: '/dashboard', icon: '/icons/home.svg', subMenuItems: [] },
+    {name: 'Students', to: '/dashboard', icon: '/icons/about.svg', subMenuItems: [{ name: 'All Students', to: ''},        
+    { name: 'Mandarian Speaking', to: ''},
+    { name: 'Portoguese Speaking', to: ''},
+    { name: 'Europeans', to: ''},
+    { name: 'Test', to: ''},
+    { name: 'Uncatorized', to: ''}] },
+    {name: 'Tasks', to: '', icon: '/icons/destinations.svg', 
+      subMenuItems: [
+      ] },
+    {name: 'Courses', to: '/dashboard', icon: '/icons/blog.svg', subMenuItems: [] },
+    {name: 'Insurance', to: '/dashboard', icon: '/icons/services.svg', subMenuItems: [{ name: 'Quote and Buy', to: '/canada'},        
+    { name: 'Students', to: '/brazil'},
+    { name: 'Agent Voices', to: '/india'},
+    { name: 'Commission and Invoices', to: '/australia'},
+    { name: 'Credit and Card Payments', to: '/kenya'},
+    ] },
+    {name: 'Enroll & Book', to: '/contacts', icon: '/icons/contacts.svg', subMenuItems: [] },
+    {name: 'Accounting', to: '/contacts', icon: '/icons/contacts.svg', subMenuItems: [{ name: 'Payments', to: '/canada'},        
+    { name: 'Invoices', to: '/brazil'},
+    { name: 'Refunds', to: '/india'},
+    { name: 'Sub-Agents', to: '/australia'},
+    { name: 'Intl. Transfer', to: '/kenya'},
+    { name: 'Credit Card Payouts', to: '/moldova'}] },
+    {name: 'Marketing', to: '/contacts', icon: '/icons/contacts.svg', subMenuItems: [{ name: 'Payments', to: '/canada'},        
+    { name: 'Forms', to: '/brazil'},
+    { name: 'Templates', to: '/india'},
+    { name: "Automation", to: '/australia'},
+    { name: 'Website', to: '/kenya'},
+    { name: 'Goals', to: '/moldova'},{ name: 'Segments', to: '/moldova'}] },
+    {name: 'Reports', to: '/blog', icon: '/icons/blog.svg', subMenuItems: [] },
+    {name: 'Analytics', to: '/blog', icon: '/icons/blog.svg', subMenuItems: [] },
+    
+    
+  ];
+
+  const fonts = {
+    header: 'Roboto Slab',
+    menu: 'Roboto Slab'
+  }
     return (
-        <div>
-            <SideBar/>
+        <s.App>
+            <SideBar backgroundImage={backgroundImage}
+        sidebarHeader={sidebarHeader}
+        menuItems={menuItems}
+        fonts={fonts}
+        colorPalette={Palette.brown}/>
             <main>{children}</main>
            
-        </div>
+        </s.App>
     )
 }
--- src/layout/sidebar/SideBar.js
+++ src/layout/sidebar/SideBar.js
@@ -1,263 +1,232 @@
-import React from 'react'
-import "./sidebar.style.css"
+import React, { useState, useEffect, useLayoutEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
+import * as s from './sidebar.styles';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export const SideBar = () => {
-    return (<div className="sidebar-mini layout-fixed">
-        {/*-- Main Sidebar Container */}
-      <div>
-      <aside className="main-sidebar sidebar-dark-primary elevation-4">
- {/*-- Brand Logo */}
- <a href="index3.html" className="brand-link">
-   <img src="images/logo.png" alt="Studyroo-logo" className="img-fluid"
-      />
-  
- </a>
+const Sidebar = props => {
+  const { 
+    backgroundImage = '', 
+    sidebarHeader = {
+      fullName: '',
+      shortName: ''
+    },
+    menuItems = [],
+    fonts = {
+      header: '',
+      menu: ''
+    },
+    colorPalette = {
+      bgColor1: 'rgba(11, 171, 100, 0.8)',
+      bgColor2: 'rgba(59, 183, 143, 0.8)',
+      fontColor: 'rgba(22, 46, 39)',
+      fontColorSelected: 'rgba(255, 255, 255)',
+      dividerColor: 'rgba(122, 204, 178)',
+      selectedBackgroundCollapsedMode: 'dark'
+    }
+  } = props;
 
- {/*-- Sidebar */}
- <div className="sidebar">
-   {/*-- Sidebar user panel (optional) */}
-   <div className="user-panel ">
-     <div className="image">
-       <img src="images/admin.svg" className="img-circle elevation-2" alt="User Image"/>
-     </div>
-     <div className="info">
-       <a href="#" className="d-block">Artur Szulakowski </a>
-       <span>MANAGING DIRECTOR</span>
-     </div>
-   </div>
+  // State
+  const [selected, setSelectedMenuItem] = useState(menuItems[0].name);
+  const [isSidebarOpen, setSidebarState] = useState(true);
+  const [header, setHeader] = useState(sidebarHeader.fullName);
+  const [subMenusStates, setSubmenus] = useState({})
 
-   {/*-- Sidebar Menu */}
-   <nav className="mt-2">
-     <ul className="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-       {/*-- Add icons to the links using the .nav-icon className
-            with font-awesome or any other icon font library */}
-       <li className="nav-item">
-         <a href="#" className="nav-link active">
-           <i className="nav-icon fas fa-tachometer-alt"></i>
-           <p>
-             Dashboard
-           </p>
-         </a>
-        
-       </li>
-     
-       <li className="nav-item has-treeview">
-         <a href="#" className="nav-link">
-          <i className="fal fa-user-graduate"></i>
-           <p>
-              Students
-             <i className="fas fa-angle-left right"></i>
-             
-           </p>
-         </a>
-         <ul className="nav nav-treeview">
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Top Navigation</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Top Navigation</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Boxed</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Fixed Sidebar</p>
-             </a>
-           </li>
-          
-         </ul>
-       </li>
-       <li className="nav-item">
-         <a href="#" className="nav-link">
-           <i className="fal fa-tasks"></i>
-           <p>
-             Task
-           </p>
-         </a>
-       </li>
-        <li className="nav-item">
-         <a href="#" className="nav-link">
-              <i className="fas fa-rss-square"></i>
-           <p>
-             News Feed
-           </p>
-         </a>
-       </li>
-        <li className="nav-item">
-         <a href="#" className="nav-link">
-           <i className="fal fa-book-alt"></i>
-           <p>
-             Courses
-           </p>
-         </a>
-       </li>
-       <li className="nav-item has-treeview">
-         <a href="#" className="nav-link">
-           <i className="fal fa-house-flood"></i>
-           <p>
-             Insurance
-             <i className="fas fa-angle-left right"></i>
-           </p>
-         </a>
-         <ul className="nav nav-treeview">
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>General</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Icons</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Buttons</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Sliders</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Modals & Alerts</p>
-             </a>
-           </li>
-          
-         </ul>
-       </li>
-       <li className="nav-item">
-         <a href="#" className="nav-link">
-          <i className="far fa-window-restore"></i>
-           <p>
-             Accommodation
-           </p>
-         </a>
-       </li>
-        <li className="nav-item">
-         <a href="#" className="nav-link">
-         <i className="fas fa-book"></i>
-           <p>
-            Enroll Book
-           </p>
-         </a>
-       </li>
+  // Effects
 
-       <li className="nav-item has-treeview">
-         <a href="#" className="nav-link">
-         <i className="far fa-calculator"></i>
-           <p>
-            Accounting
-             <i className="fas fa-angle-left right"></i>
-           </p>
-         </a>
-         <ul className="nav nav-treeview">
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>General Elements</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Advanced Elements</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Editors</p>
-             </a>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Validation</p>
-             </a>
-           </li>
-         </ul>
-       </li>
-      
-       <li className="nav-item has-treeview">
-         <a href="#" className="nav-link">
-          <i className="fas fa-laptop-code"></i>
-           <p>
-             Marketing
-             <i className="right fas fa-angle-left"></i>
-           </p>
-         </a>
-         <ul className="nav nav-treeview">
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Level 2</p>
-             </a>
-           </li>
-           <li className="nav-item has-treeview">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>
-                 Level 2
-                 <i className="right fas fa-angle-left"></i>
-               </p>
-             </a>
-             <ul className="nav nav-treeview">
-               <li className="nav-item">
-                 <a href="#" className="nav-link">
-                   <i className="far fa-dot-circle nav-icon"></i>
-                   <p>Level 3</p>
-                 </a>
-               </li>
-               <li className="nav-item">
-                 <a href="#" className="nav-link">
-                   <i className="far fa-dot-circle nav-icon"></i>
-                   <p>Level 3</p>
-                 </a>
-               </li>
-               <li className="nav-item">
-                 <a href="#" className="nav-link">
-                   <i className="far fa-dot-circle nav-icon"></i>
-                   <p>Level 3</p>
-                 </a>
-               </li>
-             </ul>
-           </li>
-           <li className="nav-item">
-             <a href="#" className="nav-link">
-               <i className="far fa-circle nav-icon"></i>
-               <p>Level 2</p>
-             </a>
-           </li>
-         </ul>
-       </li>
-      
- 
-     </ul>
-   </nav>
-   {/*-- /.sidebar-menu */}
- </div>
- {/*-- /.sidebar */}
-</aside>
- </div>
- </div>
+  // Set selected menu item based on URL pathname
+  useLayoutEffect(() => {
+    const path = window.location.pathname;
+    const parts = path.split('/');
+
+    if (path !== '/' && parts[1].charAt(0).toUpperCase() !== menuItems[0].name) {
+      const selectedItem = parts[1].charAt(0).toUpperCase() + parts[1].slice(1);
+      setSelectedMenuItem(selectedItem)
+    }
+  }, [menuItems])
+
+  // Update of header state
+  useEffect(() => {
+    isSidebarOpen ? setTimeout(() => setHeader(sidebarHeader.fullName), 200) : setHeader(sidebarHeader.shortName);
+  }, [isSidebarOpen, sidebarHeader])
+
+
+  // Update of sidebar state
+  useEffect(() => {
+    const updateWindowWidth = () => {
+      if (window.innerWidth < 1280) setSidebarState(false);
+      else setSidebarState(true)
+    }
+
+    window.addEventListener('resize', updateWindowWidth);
+
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  }, [isSidebarOpen]);
+
+
+  // Add index of items that contain sub menu items
+  useEffect(() => {
+    const newSubmenus = {};
+
+    menuItems.forEach((item, index) => {
+      const hasSubmenus = !!item.subMenuItems.length;
+
+      if (hasSubmenus) {
+        newSubmenus[index] = {};
+        newSubmenus[index]['isOpen'] = false;
+        newSubmenus[index]['selected'] = null;
+      }
+    })
+
+
+    // Set selected submenu if user landed on one
+    const path = window.location.pathname;
+    const parts = path.split('/');
+
+    if (parts.length === 3) {
+      const selectedItem = parts[1].toLowerCase();
+      const selectedSubItem = parts[2].toLowerCase()
+      const selectedItemIndex = menuItems.findIndex(item => item.name.toLowerCase() === selectedItem);
+      const selectedSubItemIndex = menuItems[selectedItemIndex]?.subMenuItems.findIndex(subItem => subItem.name.toLowerCase() === selectedSubItem);
+
+      if (selectedItemIndex !== -1) newSubmenus[selectedItemIndex]['isOpen'] = true;
+      if (selectedItemIndex !== -1 && selectedSubItemIndex !== -1) newSubmenus[selectedItemIndex]['selected'] = selectedSubItemIndex;
+    }
+
+    Object.keys(subMenusStates).length === 0 && setSubmenus(newSubmenus);
+  }, [menuItems, subMenusStates]);
+
+  const handleMenuItemClick = (name, index) => {
+    setSelectedMenuItem(name);
+
+    const subMenusCopy = JSON.parse(JSON.stringify(subMenusStates));
+
+    if (subMenusStates.hasOwnProperty(index)) { 
+      subMenusCopy[index]['isOpen'] = !subMenusStates[index]['isOpen'] 
+      setSubmenus(subMenusCopy)
+    }
+    else {
+      for (let item in subMenusStates) {
+        subMenusCopy[item]['isOpen'] = false;
+        subMenusCopy[item]['selected'] = null
+      }
+      setSubmenus(subMenusCopy);
+    }
+  }
+
+  const handleSubMenuItemClick = (menuItemIdx, subMenuItemIdx) => {
+    const subMenusCopy = JSON.parse(JSON.stringify(subMenusStates));
+
+    subMenusCopy[menuItemIdx]['selected'] = subMenuItemIdx;
+    setSubmenus(subMenusCopy);
+  }
+
+
+  const menuItemsJSX = menuItems.map((item, index) => {
+    const isItemSelected = selected === item.name;
+
+    const hasSubmenus = !!item.subMenuItems.length;
+    const isOpen = subMenusStates[index]?.isOpen;
+
+    const subMenusJSX = item.subMenuItems.map((subMenuItem, subMenuItemIndex) => {
+      const isSubmenuItemSelected = subMenusStates[index]?.selected === subMenuItemIndex;
+
+      return (
+        <Link to={`${item.to}${subMenuItem.to}`} style={{ textDecoration: 'none' }} key={subMenuItemIndex}>
+          <s.SubMenuItem
+            onClick={() => handleSubMenuItemClick(index, subMenuItemIndex)}
+            selected={isSubmenuItemSelected}
+            colorPalette={colorPalette}
+          >
+            {subMenuItem.name}
+          </s.SubMenuItem>
+        </Link>
+      )
+    })
+
+    return (
+      <s.ItemContainer key={index}>
+        <Link to={item.to} style={{ textDecoration: 'none' }}>
+          <s.MenuItem           
+            font={fonts.menu}
+            selected={isItemSelected}
+            onClick={() => handleMenuItemClick(item.name, index)}
+            isSidebarOpen={isSidebarOpen}
+            isOpen={isOpen}
+            colorPalette={colorPalette}
+          >
+           <s.Icon isSidebarOpen={isSidebarOpen} src={item.icon}></s.Icon> 
+            <s.Text isSidebarOpen={isSidebarOpen}>{item.name}</s.Text>
+            {hasSubmenus && isSidebarOpen && (
+              <s.DropdownIcon selected={isItemSelected} isOpen={isOpen} colorPalette={colorPalette} />
+            )}
+          </s.MenuItem>
+        </Link>
+
+        {/* Display submenus if they exist  */}
+        <AnimatePresence>
+          {hasSubmenus && isOpen && (
+            <motion.nav 
+              initial={{ opacity: 0, y: -15 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.35 }}
+              exit={{ opacity: 0, x: -30 }}
+            >
+              <s.SubMenuItemContainer isSidebarOpen={isSidebarOpen} colorPalette={colorPalette}>{subMenusJSX}</s.SubMenuItemContainer>
+            </motion.nav>
+          )}
+        </AnimatePresence>
+      </s.ItemContainer>
     )
+  });
+
+  return (
+    <s.SidebarContainer backgroundImage={backgroundImage} isSidebarOpen={isSidebarOpen} colorPalette={colorPalette}>
+      {isSidebarOpen && <s.SidebarHeader font={fonts.header}><a href="index3.html" class="brand-link">
+      <img src="images/logo.png" alt="Studyroo-logo" class="img-fluid"
+         />
+     
+    </a>
+
+    
+      
+      <div class="user-panel ">
+        <div class="image">
+          <img src="images/admin.svg" class="img-circle elevation-2" alt="User Image"/>
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Artur Szulakowski </a>
+          <span>MANAGING DIRECTOR</span>
+        </div>
+      </div><s.TogglerContainer onClick={() => setSidebarState(!isSidebarOpen)}>
+        <s.Toggler />
+      </s.TogglerContainer></s.SidebarHeader>}
+      {!isSidebarOpen && <s.SidebarHeader font={fonts.header}><a href="index3.html" class="brand-link">
+      <img src="images/logo.png" alt="Studyroo-logo" class="img-fluid"
+         />
+     
+    </a>
+
+    
+      
+      <div class="user-panel ">
+        <div class="image">
+          <img src="images/admin.svg" class="img-circle elevation-2" alt="User Image"/>
+        </div>
+        <div class="info">
+          <a href="#" class="d-block">Artur Szulakowski </a>
+          <span>MANAGING DIRECTOR</span>
+        </div>
+      </div><s.TogglerContainer onClick={() => setSidebarState(!isSidebarOpen)}>
+        <s.Toggler />
+      </s.TogglerContainer></s.SidebarHeader>}
+                         
+                         
+      
+      <s.MenuItemContainer>{menuItemsJSX}</s.MenuItemContainer>
+      
+    </s.SidebarContainer>
+  )
 }
+
+export default Sidebar
