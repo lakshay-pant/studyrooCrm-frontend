b9ce35c6259cfc5c8975eeaccc05794302a4dcde
lakshay-pant <pantlakshay2@gmail.com>
2021-03-16 10:45:41 +0530

    hey added

---
+++ src/pages/allStudents/allStudentSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  students: [],
+  isLoading: false,
+  error: "",
+  
+};
+
+const studentListSlice = createSlice({
+  name: "studentList",
+  initialState,
+  reducers: {
+    fetchStudentLoading: (state) => {
+      state.isLoading = true;
+    },
+    fetchStudentSuccess: (state, action) => {
+      state.tickets = action.payload;
+      state.isLoading = false;
+    },
+    fetchStudentFail: (state, { payload }) => {
+      state.isLoading = false;
+      state.error = payload;
+    },
+    
+  },
+});
+
+const { reducer, actions } = studentListSlice;
+
+export const {
+  fetchStudentLoading,
+  fetchStudentSuccess,
+  fetchStudentFail,
+} = actions;
+
+export default reducer;
